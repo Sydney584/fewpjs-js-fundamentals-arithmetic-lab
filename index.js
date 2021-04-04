3cc40987c4eeacfe68d999b68d9c22f459f44813
sydney <sydneychase5841@gmail.com>
2021-04-04 16:05:05 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+
+const random = 5;
+Math.random();
+Number.isInteger(30);
+
+const num3 = 39;
+const num4 = 7;
+const mod = 39 % 7;
+
+Math.max = (5, 10, 15, 20);
+const max = 20;
 
 // Write your code here
